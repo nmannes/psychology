3de1f768682a174487324bae8e3912fa255b5d39
Nathan Mannes <nmannes@gmail.com>
2023-11-10 19:27:50 -0600

    annotate models

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: tests
+#
+#  id         :integer          not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  age        :integer
+#  gender     :string
+#  data       :json
+#  user_id    :integer          not null
+#
 class Test < ApplicationRecord
 end
--- test/models/test_test.rb
+++ test/models/test_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: tests
+#
+#  id         :integer          not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  age        :integer
+#  gender     :string
+#  data       :json
+#  user_id    :integer          not null
+#
 require "test_helper"
 
 class TestTest < ActiveSupport::TestCase
