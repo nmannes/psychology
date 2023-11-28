958bf22471de26f04460779521fcd6752ebcc0a0
Nathan Mannes <nmannes@gmail.com>
2023-11-27 21:39:56 -0600

    redirect to the lab instead of the homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 class HomeController < ApplicationController
-  def index; end
+  def index
+    if current_user
+      redirect_to '/lab'
+    end
+  end
 
   def logout
     redirect_to '/'
