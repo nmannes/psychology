a7b8ab36b25f3ce85648115a06d30d1f153473ea
Nathan Mannes <nmannes@gmail.com>
2023-12-01 13:39:58 -0600

    added a client table

---
+++ db/migrate/20231201193917_create_clients.rb
@@ -0,0 +1,12 @@
+class CreateClients < ActiveRecord::Migration[7.0]
+  def change
+    create_table :clients do |t|
+      t.integer :age
+      t.string :education
+      t.references :user, null: false, foreign_key: true
+      t.references :test, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
